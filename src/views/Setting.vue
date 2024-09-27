e8b6cb65a523ecb342952ade121b49acbc8ad672
Willy-WZW <joystick449@gmail.com>
2024-09-27 12:59:39 +0800

    0927 早上

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -148,7 +148,9 @@ export default {
                     <div class="menuMain">
                         <div class="addMenuDiv">+&nbsp&nbsp新增餐點</div>
                         <div class="menuItem">
-                            <div class="itemPic"> </div>
+                            <div class="itemPic">
+                                <i class="fa-solid fa-upload"></i>
+                            </div>
                             <div class="itemName">青醬蛤蠣義大利麵</div>
                             <div class="itemPrice">$ 250</div>
                             <div class="itemWorksta">
@@ -182,62 +184,11 @@ export default {
                             <div class="saveBtn">儲存</div>
                         </div>
                     </div>
-                    <div class="menuMain"></div>
+                    <div class="custItem">
+                        <div class="addItem">+&nbsp&nbsp新增客製化選項</div>
+                    </div>
                 </div>
             </div>
-
-
-            <!-- <div class="mealNameArea">
-                <span>餐點名稱:</span>
-                <input type="text" placeholder="請輸入餐點名稱">
-                <span>餐點類型:</span>
-                <select name="">
-                    <option value="未選擇">未選擇</option>
-                    <option value="漢堡">漢堡</option>
-                    <option value="沙拉">沙拉</option>
-                    <option value="飲料">飲料</option>
-                    <option value="義大利麵">義大利麵</option>
-                    <option value="組合餐">組合餐</option>
-                </select>
-                <span>餐點價格:</span>
-                <input class="price" type="text" placeholder="輸入餐點價格">
-            </div>
-            <div class="mealImage">
-                <span>新增餐點圖片:</span>
-                <input type="file" name="" id="">
-            </div>
-            <div class="mealDescription">
-                <textarea class="mealDes" style="resize: none;" placeholder="新增餐點描述"></textarea>
-            </div>
-            <el-collapse v-model="activeNames">
-                <el-collapse-item v-for="(collapse, index) in collapses" :key="index" :title="collapse.title"
-                    :name="collapse.name"> -->
-            <!-- 自定義 title 部分，使用者可以輸入 -->
-            <!-- <template #title>
-                        <input class="collTitle" type="text" v-model="collapse.title"
-                            :placeholder="'客製化選項標題' + (index + 1)" @input="updateTitle(collapse, index)" @click.stop />
-                    </template>
-<div class="editOption">
-    <select v-model="collapse.type" @change="clearOption(collapse)" name="" id="">
-        <option value="radio">單選</option>
-        <option value="checkbox">多選</option>
-    </select>
-    <i class="fa-solid fa-plus" @click="addOptionBox(collapse)"></i>
-    <i class="fa-solid fa-file-circle-xmark" @click="deleteThisCollapse(index)"></i>
-</div>
-<div class="option" v-for="(option, opIndex) in collapse.options" :key="opIndex">
-    <input v-if="collapse.type === 'radio'" type="radio" :name="'radio' + collapse.name" id="">
-    <input v-if="collapse.type === 'checkbox'" type="checkbox" name="" id="">
-    <input type="text" class="markupOption" v-model="option.text" :placeholder="'客製化選項' + (opIndex + 1)"> -->
-            <!-- markup防呆: 1. + or - 2.僅能是數字 3.數字不能0開頭 -->
-            <!-- <input type="text" class="markup" placeholder="金額+/-">
-                        <i class="fa-solid fa-trash" @click="removeOption(collapse, opIndex)"></i>
-                    </div>
-                </el-collapse-item>
-            </el-collapse>
-            <div class="addOption">
-                <i class="fa-solid fa-square-plus" @click="addCustomization()"></i>
-            </div> -->
         </div>
     </div>
 </template>
@@ -245,6 +196,8 @@ export default {
 <style scoped lang="scss">
 $divColor: #fff;
 $addDiv: #343a3f;
+$suppliable: #1ce34e;
+$soldOut: #e02d11;
 
 .big {
     width: 100%;
@@ -546,8 +499,15 @@ $addDiv: #343a3f;
 
                         .itemPic {
                             grid-area: 1 / 1 / 5 / 7;
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
                             border: dotted;
                             margin: 4% 4% 0 4%;
+
+                            .fa-upload {
+                                font-size: 30px;
+                            }
                         }
 
                         .itemName {
@@ -593,31 +553,34 @@ $addDiv: #343a3f;
                             display: flex;
 
                             .itemStatus {
-                                width: 60%;
+                                width: 50%;
                                 margin-right: 5%;
                                 margin-bottom: 2%;
                                 border-radius: 5px;
                                 letter-spacing: 3px;
                                 cursor: pointer;
-                                font-size: 25px;
+                                font-size: 17px;
                                 font-weight: bold;
                                 color: white;
-                                background-color: green;
-                                border: 1px solid green;
+                                background-color: $suppliable;
+                                border: 1px solid $suppliable;
                                 display: flex;
                                 justify-content: center;
                                 align-items: center;
                                 transition: transform 0.7s, background-color 0.5s;
                             }
+
                             .flip {
                                 transform: rotateX(360deg);
-                                background-color: red;
+                                background-color: $soldOut;
+                                border-color: $soldOut;
                                 color: white;
                             }
 
 
                             .itemIcon {
                                 width: 35%;
+                                margin-left: 8%;
                                 display: flex;
                                 justify-content: space-between;
                                 align-items: center;
@@ -707,146 +670,32 @@ $addDiv: #343a3f;
                         }
                     }
                 }
+
+                .custItem {
+                    width: 97%;
+                    height: 65%;
+                    display: flex;
+                    justify-content: start;
+                    align-items: center;
+                    overflow-x: scroll;
+                    white-space: nowrap;
+
+                    .addItem {
+                        min-width: 25%;
+                        height: 100%;
+                        margin-right: 3%;
+                        border-radius: 10px;
+                        font-size: 25px;
+                        font-family: "Noto Sans TC", sans-serif;
+                        color: white;
+                        background-color: $addDiv;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                    }
+                }
             }
         }
-
-        // .mealNameArea {
-        //     width: 90%;
-        //     padding-bottom: 5px;
-        //     font-size: 20px;
-        //     border-bottom: 2px solid black;
-
-        //     span {
-        //         margin-right: 5px;
-        //     }
-
-        //     input {
-        //         width: 50%;
-        //         border: none;
-        //         color: transparent;
-        //         font-size: 20px;
-        //         margin-right: 30px;
-        //         color: black;
-        //     }
-
-        //     select {
-        //         font-size: 20px;
-        //         margin-right: 20px;
-        //     }
-
-        //     .price {
-        //         width: 120px;
-        //         color: black;
-        //     }
-        // }
-
-        // .mealImage {
-        //     width: 90%;
-        //     margin-top: 5px;
-
-        //     input {
-        //         cursor: pointer;
-        //         width: 70px;
-        //     }
-
-        //     span {
-        //         margin-right: 5px;
-        //     }
-        // }
-
-        // .mealDescription {
-        //     width: 90%;
-        //     margin-top: 5px;
-
-        //     .mealDes {
-        //         width: 80%;
-        //         height: 80px;
-        //         font-size: 18px;
-        //     }
-        // }
-
-        // .collTitle {
-        //     width: 230px;
-        //     font-size: 26px;
-        //     margin-left: 10px;
-        // }
-
-        // .el-collapse {
-        //     width: 90%;
-        //     margin: 30px auto;
-        //     --el-collapse-header-font-size: 20px;
-        //     --el-collapse-header-bg-color: #b0f079;
-
-        //     .el-collapse-item {
-        //         margin-bottom: 10px;
-        //     }
-
-        //     .editOption {
-        //         width: 100%;
-        //         display: flex;
-        //         justify-content: space-around;
-        //         align-items: center;
-        //         margin: 2% auto;
-
-        //         select {
-        //             font-size: 30px;
-        //         }
-
-        //         .fa-plus {
-        //             font-size: 40px;
-        //             cursor: pointer;
-        //         }
-
-        //         .fa-file-circle-xmark {
-        //             font-size: 40px;
-        //             cursor: pointer;
-        //         }
-
-        //     }
-
-        //     .option {
-        //         width: 100%;
-        //         margin: 2% auto;
-        //         display: flex;
-        //         align-items: center;
-
-        //         input[type=checkbox],
-        //         input[type=radio] {
-        //             margin: 0 30px;
-        //             scale: 2;
-        //         }
-
-        //         .markupOption {
-        //             width: 60%;
-        //             font-size: 40px;
-        //             margin-right: 100px
-        //         }
-
-        //         .markup {
-        //             width: 15%;
-        //             font-size: 40px;
-        //             margin-right: 70px;
-        //         }
-
-        //         .fa-trash {
-        //             font-size: 40px;
-        //             cursor: pointer;
-        //         }
-        //     }
-        // }
-
-        // .addOption {
-        //     width: 90%;
-        //     margin-top: 30px;
-        //     display: flex;
-        //     justify-content: center;
-
-        //     .fa-square-plus {
-        //         font-size: 40px;
-        //         cursor: pointer;
-        //     }
-        // }
     }
-
 }
 </style>
